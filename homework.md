19df6c99243f00bf9e500c499ceefe2dd364b7f1
Girdhar Gohel <girdhardeveloper9@gmail.com>
2026-04-29 20:01:33 +0530

    exploring express next

--- homework.md
+++ homework.md
@@ -18,3 +18,7 @@
 - _Order of the routes matters a lot_
 - Install postman app and make a workspace/collection > test API call
 - Write logic to handle GET, POST, PATCH, DELETE API calls and test them on postman
+- Explore routing and use of ?, +, (), \* in the routes
+- Use of regex in routes /a/ , /.\*fly$/
+- Reading the query params in the routes
+- Reading the dynamic routes
--- src/app.js
+++ src/app.js
@@ -2,10 +2,38 @@ const express = require("express");
 
 const app = express();
 
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("handling the route user!");
+    // res.send("I am response 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the route user 2!!");
+    // res.send("I am response 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the route user 3!!!");
+    // res.send("I am response 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the route user 4!!!!");
+    // res.send("I am response 4");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the route response 5 !!!!!!");
+    res.send("I am response 5");
+  },
+);
+
 // This will handle only get call to user
-app.get("/user", (req, res) => {
-  res.send({ firsname: "Girdhar", lastname: "Gohel" });
-});
+// app.get("/user", (req, res) => {
+//   res.send({ firsname: "Girdhar", lastname: "Gohel" });
+// });
 
 app.post("/user", (req, res) => {
   res.send("Data saved successfully to the database");
@@ -15,14 +43,19 @@ app.delete("/user", (req, res) => {
   res.send("Data deleted successfully");
 });
 
-app.use("/user", (req, res) => {
-  res.send("default for handling url");
-});
+// app.use("/user", (req, res) => {
+//   res.send("default for handling url");
+// });
 
 // This will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hi, I am tester tester!");
-});
+// app.use("/test", (req, res) => {
+//   res.send("Hi, I am tester tester!");
+// });
+
+// app.get("/user/:userId/:name/:password", (req, res) => {
+//   console.log(req.params);
+//   res.send({ firsname: "Girdhar" });
+// });
 
 // app.use("/", (req, res) => {
 //   res.send("Hello World...!");
